ba3be0ce8025b1168428fc8236b1c5777e358235
Akhil Puri <ap4723@srmist.edu.in>
2026-02-10 16:14:16 +0530

    [Akhil] Added: UC3: Storing mapped elements

--- StreamAPI.java
+++ StreamAPI.java
@@ -1,18 +1,15 @@
 /**
- * STREAM-API UC2 - Ability to Transform Each Element to Double
- * and Store the Result.
+ * STREAM-API UC3 - Ability to store the Transformed doublevalue into a new List
  *
  * In this program:
  * 1. A Stream is created from a List using stream().
  * 2. The map() intermediate operation is used to transform
  *    each element by doubling its value.
- * 3. The forEach() terminal operation is used to display
- *    transformed elements directly.
- * 4. The collect() terminal operation is used to store the
+ * 3. The collect() terminal operation is used to store the
  *    transformed elements into a new List.
  *
  * @Developer
- * @version2.2
+ * @version2.3
  */
 
 import java.util.*;
@@ -21,11 +18,6 @@ import java.util.stream.*;
 public class StreamAPI {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
-
-        // no storing directly processing.
-        numbers.stream()
-                .map(x -> x * 2)
-                .forEach(System.out::println);
 
         // storing of elements.
         List<Integer> doubled = numbers.stream()
