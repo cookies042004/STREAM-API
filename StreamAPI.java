ba3768a01fc0a676de2b64c0dd9b1a2e5ead7852
Akhil Puri <ap4723@srmist.edu.in>
2026-02-11 11:35:29 +0530

    [Akhil] Added: UC6: MIN and MAX Even Numbers

--- StreamAPI.java
+++ StreamAPI.java
@@ -1,6 +1,5 @@
 /**
- * STREAM-API UC5 - Ability to peak and show the first even number in
- * the number stream.
+ * STREAM-API UC6 - Ability to find min and max even number in the number stream.
  *
  In this program:
  * 1. A Stream is created from a List using stream().
@@ -10,22 +9,27 @@
  *    the first matching element from the stream.
  *
  * @Developer
- * @version: 2.5
+ * @version: 2.6
  */
 
-import java.util.*;
-import java.util.stream.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 public class StreamAPI {
     public static void main(String[] args) {
 
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 8, 10);
 
-        // Storing only even numbers into a new optional
-        Optional<Integer> even = numbers.stream()
+        Optional<Integer> minEven = numbers.stream()
                 .filter(x -> x % 2 == 0)
-                .findFirst();
+                .min(Integer::compareTo);
 
-        System.out.println(even);
+        Optional<Integer> maxEven = numbers.stream()
+                .filter(x -> x % 2 == 0)
+                .max(Integer::compareTo);
+
+        minEven.ifPresent(x -> System.out.println("Minimum Even: " + x));
+        maxEven.ifPresent(x -> System.out.println("Maximum Even: " + x));
     }
 }
